1afabfad9bddf668d02cedb47236a9d8725d8fd9
Hugo G RIVAS <hugo.rivas@vun.mx>
2018-07-18 18:15:35 -0500

    1807181815

--- Edicion/src/main/java/com/kaltia/kaltiatransaccion/Edicion/WS/WSEdicion.java
+++ Edicion/src/main/java/com/kaltia/kaltiatransaccion/Edicion/WS/WSEdicion.java
@@ -1,10 +1,10 @@
 package com.kaltia.kaltiatransaccion.Edicion.WS;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kaltia.kaltiatransaccion.Edicion.VO.VOStatus;
@@ -15,6 +15,7 @@ public class WSEdicion {
 //	@Autowired
 //	VOStatus voStatus;
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@RequestMapping(
             method = RequestMethod.POST, path = "/{modelo}/{seccion}",  consumes = "application/json", produces = "application/json")
 	public  VOStatus updateEdicion(@PathVariable String modelo, @PathVariable String seccion, @RequestBody Object objeto) {
